ab4dfdaa19cfd9e630141459531753b07241d8b6
Filipe Silva <52262356+filipe-tti@users.noreply.github.com>
2023-12-04 20:52:05 -0300

    criando navbar e footer e adcionando estilos gerais

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 // PAGES
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -14,12 +16,18 @@ function App() {
     <div>
 
       <BrowserRouter>
+      
+        <Navbar/>
+
         <div className="container">
           <Routes>
              <Route path="/" element= {<Home/>} />
              <Route path="/about" element= {<About/>} />
           </Routes>
         </div>
+
+        <Footer/>
+
       </BrowserRouter>
       
     </div>
