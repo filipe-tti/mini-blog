7541568b836412476c3aa380b7ee6e5292fb777d
Filipe Silva <52262356+filipe-tti@users.noreply.github.com>
2024-07-07 18:45:27 -0300

    finalizando deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mini_blog_react_deploy"
+  base: "/mini_blog"
 })
